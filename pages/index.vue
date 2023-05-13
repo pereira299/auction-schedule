9928e15554019afa76397c564b09cb1f05fd9f46
pereira299 <lucasper057@gmail.com>
2023-05-13 15:12:20 -0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import "static/styles/global.css"
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
